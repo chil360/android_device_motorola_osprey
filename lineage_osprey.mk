5658949f019e43ed0cd974d367b952bb87995af6
Aayush Gupta <aayushgupta219@gmail.com>
2021-04-07 10:33:01 +0100

    osprey: Make sure proprietary binaries exists before compilation
    - Required to boot the OS
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Signed-off-by: althafvly <althafvly@gmail.com>
    Change-Id: Id478ed5b013488f1209f5944e08607180bc1a64a

--- lineage_osprey.mk
+++ lineage_osprey.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from osprey device
-$(call inherit-product, device/motorola/osprey/full_osprey.mk)
+$(call inherit-product, device/motorola/osprey/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
