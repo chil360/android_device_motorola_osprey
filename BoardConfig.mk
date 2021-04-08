80af858b0ce365e8245964658c9954fa9b74f1f6
Anton Hansson <hansson@google.com>
2021-04-08 11:45:06 +0100

    osprey: Use TARGET_SCREEN_DENSITY
    This variable is used to auto-populate the ro.sf.lcd_density as
    well as selecting density bucket for resources in recovery.
    
    Bug: 118676832
    Test: adb shell getprop ro.sf.lcd_density
    Test: boot into recovery
    Change-Id: Ib383d18b61e7a2058a69ae26136aa053fdba4df2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,9 @@ DEVICE_PATH := device/motorola/osprey
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := osprey,osprey_umts,osprey_u2,osprey_ud2,osprey_uds,osprey_cdma,osprey_sprint,osprey_udstv
 
+# Display
+TARGET_SCREEN_DENSITY := 320
+
 # Kernel
 TARGET_KERNEL_CONFIG := osprey_defconfig
 
